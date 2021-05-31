07cdf92cb1064ef0832646ea2c3dd1210a94e59d
johny9647 <johny9647@gmail.com>
2021-05-31 21:13:42 +0800

    commit

---
+++ config sample.js
@@ -0,0 +1,11 @@
+module.exports = 
+{
+    apiKey: "AIzaSyBtVoaNeH4lCK6UQRktcMLShQ6IyqZ2Q0U",
+    authDomain: "irish-mlm-local.firebaseapp.com",
+    databaseURL: "https://irish-mlm-local.firebaseio.com",
+    projectId: "irish-mlm-local",
+    storageBucket: "irish-mlm-local.appspot.com",
+    messagingSenderId: "866848679724",
+    appId: "1:866848679724:web:7410d960df56d8629c2e89",
+    measurementId: "G-HRVKYZ4D1F"
+};
--- functions/controllers/FrontController.js
+++ functions/controllers/FrontController.js
@@ -1,11 +1,12 @@
-const ACCOUNT_CLASS = require('../classess/AccountClass');
+// const ACCOUNT_CLASS = require('../classess/AccountClass');
+const {HTTPS_ERROR}= require('../plugin/firebase');
 module.exports =
 {
     async registration(data, context)
     {
         // const ACCOUNT = new ACCOUNT_CLASS();
         // return await ACCOUNT.createAccount(data);
-        // console.log('gago');
+        console.log('gago');
         
     },
 };
